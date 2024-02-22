4dd3b477e595364c2049082b3e0cd771de3aef85
Jay <naturesky0411@gmail.com>
2024-02-22 22:20:30 +0900

    today is finished

--- src/pages/residences/residences.js
+++ src/pages/residences/residences.js
@@ -15,7 +15,7 @@ export default function Residences() {
         <div>
             <img src={ BackgroundIamge1} className="inset-0 bg-black opacity-50"/>
             <div className="px-4 content-center z-10 relative" style={{ backgroundColor: "#153644" }} >
-                <div className="content-center p-10 py-10">
+                <div className="content-center py-10">
                     <div className="m-auto">
                         <p className="text-sm font-GTPressura-Bold text-center text-white pb-5">VISION</p>
                     </div>
@@ -34,15 +34,15 @@ export default function Residences() {
             <img src={ BackgroundIamge2}/>
 
 
-            <div className="px-8 content-center py-10 z-10 relative" style={{ backgroundColor: "#153644" }} >
-                <div className="content-center p-12">
+            <div className="px-4 content-center py-10 z-10 relative" style={{ backgroundColor: "#153644" }} >
+                <div className="content-center py-12">
                     <div className="flex items-center">
                         <div className="w-auto mx-auto xl:w-1/3">
                             <p className="text-base font-GTPressura-Light text-center text-white px-4">As an homage to Huron Street’s history of splendid gardens, the modern, luxury condo’s interiors fuse greenery with timeless materials. The street’s verdant past comes to life as a unique feature of The Huron, with more than 23,000 square feet of private outdoor space spread across more than a third of the residences.</p>
                         </div>               
                     </div>
                 </div>
-                <div className="content-center p-12 xl:w-[1280px] m-auto">
+                <div className="content-center py-12 xl:w-[1280px] m-auto">
                     <div className="m-auto mb-10 xl:w-[900px]">
                         <p className="text-center font-GTPressura-Light text-4xl text-white px-4">“The interiors are inspired by Greenpoint’s golden age of industrialism and rich history of manufacturing and craftsmanship.”</p>
                     </div>
@@ -56,7 +56,7 @@ export default function Residences() {
                         <div className=" w-full right-0 sm:w-3/5">
                             <img  src={ TextImage3 }/>
                         </div>
-                        <div className="w-full sm:w-1/5 left-0 bottom-0">
+                        <div className="w-full sm:w-1/5 left-0 sm:bottom-0">
                             <p className="text-base font-GTPressura-Light text-center text-white px-4">Rooted in Greenpoint’s industrial heritage, The Huron is uniquely contemporary, functional, and inspirational. The H shaped two-tower configuration, with its long, linear connected lobby, is reminiscent of a ship with two smoke stacks.</p>
                         </div>
                     </div>
@@ -64,9 +64,78 @@ export default function Residences() {
             </div>
 
             <img src={ BackgroundIamge3}/>
+
+            <div className="px-4 content-center py-10 z-10 relative" style={{ backgroundColor: "#153644" }} >
+                <div className="content-center xl:w-[1280px] m-auto ">
+                    <div className="max-w-[1280px] w-full m-auto mt-[90px] sm:flex-row-reverse sm:flex">
+                        <div className=" w-full right-0 sm:w-3/5">
+                            <img  src={ TextImage3 }/>
+                        </div>
+                        <div className="w-full sm:w-1/5 left-0 sm:bottom-0">
+                            <p className="text-base font-GTPressura-Light text-center text-white px-4">Rooted in Greenpoint’s industrial heritage, The Huron is uniquely contemporary, functional, and inspirational. The H shaped two-tower configuration, with its long, linear connected lobby, is reminiscent of a ship with two smoke stacks.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="sm: pb-10">
+                    <div className="m-auto mb-10 xl:w-[900px]">
+                        <p className="text-center font-GTPressura-Light text-4xl text-white px-4">“The inset marble waterfall at the islands marries two of nature’s beauties – marble and wood – through craft.”</p>
+                    </div>
+                    <div className="flex items-center mt-10">
+                        <div className="w-auto mx-auto xl:w-1/3">
+                            <p className="text-base font-GTPressura-Light text-center text-white px-4">LORENZO BELLO, QUADRUM GLOBAL</p>
+                        </div>               
+                    </div>
+                </div>
+            </div>
+            
             <img src={ BackgroundIamge4}/>
+
+            <div className="px-4 content-center py-2 z-10 relative" style={{ backgroundColor: "#153644" }} >
+                <div className="content-center xl:w-[900px] m-auto ">
+                    <div className="max-w-[1280px] w-full m-auto mt-[10px]">
+                        <div className="w-full md:w-1/3 left-0 sm:bottom-0">
+                            <p className="text-base font-GTPressura-Bold text-left text-md text-white py-3 ">BEDROOM</p>
+                            <p className="text-base font-GTPressura-Light text-left text-sm text-white py-3 ">Rooted in Greenpoint’s industrial heritage, The Huron is uniquely contemporary, functional, and inspirational. The H shaped two-tower configuration, with its long, linear connected lobby, is reminiscent of a ship with two smoke stacks.</p>
+                        </div>
+                        <div className=" w-full right-0 sm:w-auto sm:mb-20">
+                            <img  src={ TextImage3 }/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <img src={ BackgroundIamge5}/>
+
+            <div className="px-4 content-center z-10 relative" style={{ backgroundColor: "#153644" }} >
+                <div className="content-center py-10">
+                    <div className="m-auto">
+                        <p className="text-sm font-GTPressura-Bold text-center text-white pb-5">VISION</p>
+                    </div>
+                    <div className="xl:w-[600px] m-auto">
+                        <p className="text-center font-GTPressura-Light text-4xl text-white px-4">The Huron: Our Vision for Greenpoint’s Newest Condos</p>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="w-auto mx-auto xl:w-1/3">
+                            <p className="text-base font-GTPressura-Light text-center text-white px-4">Rooted in the history of an ever-evolving neighborhood, The Huron is a striking tribute to Greenpoint’s heritage, interweaving past and present through stunning design and inspired creativity.</p>
+                        </div>                    
+                    </div>
+                </div>
+            </div>
+
             <img src={ BackgroundIamge6}/>
+
+            <div className="px-4 content-center z-10 relative sm:py-32" style={{ backgroundColor: "#153644" }} >
+                <div className="content-center py-10">
+                    <div className="xl:w-[970px] m-auto">
+                        <p className="text-center font-GTPressura-Light text-5xl text-white px-4">“The large windows perfectly frame endless views of New York City.”</p>
+                    </div>
+                    <div className="flex items-center pt-8">
+                        <div className="w-auto mx-auto xl:w-1/3">
+                            <p className="text-base font-GTPressura-Light text-center text-white px-4">JARED WHITE, QUADRUM GLOBAL</p>
+                        </div>                    
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
