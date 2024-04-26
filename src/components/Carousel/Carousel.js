7e5253237fa0a627bd533d11ced0934083b2969e
Jay <wmeng0103@gmail.com>
2024-04-26 17:26:24 +0900

    update

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import './Carousel.css'
 const Carousel = ({ items }) => {
 
-    const[id, setId] = useState(0);
+  const[id, setId] = useState(0);
   // const [isDragging, setIsDragging] = useState(false);
   // const [startX, setStartX] = useState(0);
   // const [scrollLeft, setScrollLeft] = useState(0);
@@ -26,40 +26,40 @@ const Carousel = ({ items }) => {
   // };
 
     const next =  () => {
-        setId(id+1);
+      setId(id+1);
     }
 
     const prev = () => {
-        setId(id-1);
+      setId(id-1);
     }
     const prevClass = id === 0 ? ' text-[#DAC2B1] ml-[20px]' : ' text-[#f2ede4] hover:text-[#b95a01] ml-[20px]';
     const nextClass = id === items.length-1 ? ' text-[#DAC2B1] ml-[20px]' : ' text-[#f2ede4] hover:text-[#b95a01] ml-[20px]'; 
 
     return (
-        <div className='py-12 max-w-[1280px] lg:flex lg:max-h-[600] lg:flex-row-reverse lg:flex-wrap lg:justify-between content-center m-auto'>
-            <div className='lg:w-[50%] px-5'>
-                <img src={items[id].image} alt=""/>
-            </div>
-            <div className='lg:w-[50%] px-5 md:pl-40'>
-                <p className="md:text-right md:w-full text-[#F1ECE2]">
-                    {items[id].text}
-                </p>
-                <div className='flex mt-3'>
-                    <div className='self-end'>
-                        <div className="flex justify-between pb-2">
-                            <div className="">
-                                <button className="flex-grow px-2" onClick={id === 0 ? null : prev }><p className={prevClass}>{'< '+' PREV '}</p></button>
-                            </div>
-                            <p className="text-[#f2ede4]"> {"  |  "}</p>
-                            <div className="page-button-next px-2">
-                                <button className="flex-grow" onClick={id === items.length-1 ? null : next}><p className={nextClass}>{' NEXT '+' >'}</p></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+      <div className='py-12 max-w-[1280px] lg:flex lg:max-h-[600] lg:flex-row-reverse lg:flex-wrap lg:justify-between content-center m-auto'>
+        <div className='lg:w-[50%] px-5'>
+          <img src={items[id].image} alt=""/>
         </div>
-    );
+        <div className='lg:w-[50%] px-5 md:pl-40'>
+          <p className="md:text-right md:w-full text-[#F1ECE2]">
+            {items[id].text}
+          </p>
+          <div className='flex mt-3'>
+            <div className='self-end'>
+              <div className="flex justify-between pb-2">
+                <div className="">
+                  <button className="flex-grow px-2" onClick={id === 0 ? null : prev }><p className={prevClass}>{'< '+' PREV '}</p></button>
+                </div>
+                <p className="text-[#f2ede4]"> {"  |  "}</p>
+                <div className="page-button-next px-2">
+                  <button className="flex-grow" onClick={id === items.length-1 ? null : next}><p className={nextClass}>{' NEXT '+' >'}</p></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 };
 
 export default Carousel;
--- src/pages/amenities/amenities.js
+++ src/pages/amenities/amenities.js
@@ -9,40 +9,40 @@ import TabImage4 from "./../../assets/img/amenities-tab-4.jpg";
 
 export default function Amenities() {
 
-    const data = [
-        {id:0, name: 'FIRST FLOOR', backgroundColor: "#2C2A2B", title: 'FIRST FLOOR', image: TabImage1, text: 'Get charged with spaces that are intuitive and elevated, inspired by purpose.'},
-        {id:1, name: 'SECOND FLOOR', backgroundColor: "#2C2A2B", title: 'SECOND FLOOR', image: TabImage2, text: 'Vibrant spaces for kids and teens, dedicated to playing and growing together.'},
-        {id:2, name: 'LOOP PARK', backgroundColor: "#2C2A2B", title: 'PARK', image: TabImage3, text: 'An imaginative 8,000+ sq ft outdoor park and playground centered around a fun treehouse.'},
-        {id:3, name: 'ROOF UP', backgroundColor: "#2C2A2B", title: 'AT THE TOP', image: TabImage4, text: 'Entertain, socialize, and relax to the backdrop of awe-inspiring views over three of the city’s boroughs and the iconic Manhattan skyline.'},
-    ];
+  const data = [
+    {id:0, name: 'FIRST FLOOR', backgroundColor: "#2C2A2B", title: 'FIRST FLOOR', image: TabImage1, text: 'Get charged with spaces that are intuitive and elevated, inspired by purpose.'},
+    {id:1, name: 'SECOND FLOOR', backgroundColor: "#2C2A2B", title: 'SECOND FLOOR', image: TabImage2, text: 'Vibrant spaces for kids and teens, dedicated to playing and growing together.'},
+    {id:2, name: 'LOOP PARK', backgroundColor: "#2C2A2B", title: 'PARK', image: TabImage3, text: 'An imaginative 8,000+ sq ft outdoor park and playground centered around a fun treehouse.'},
+    {id:3, name: 'ROOF UP', backgroundColor: "#2C2A2B", title: 'AT THE TOP', image: TabImage4, text: 'Entertain, socialize, and relax to the backdrop of awe-inspiring views over three of the city’s boroughs and the iconic Manhattan skyline.'},
+  ];
 
 
-    return (
-        <div>
-            <img src={ BackgroundIamge1} className="inset-0" alt=""/>
-            <div className="px-4 content-center z-10 relative" style={{ backgroundColor: "#2C2A2B" }} >
-                <div className="content-center py-10">
-                    <div className="m-auto">
-                        <p className="text-sm font-GTPressura-Bold text-center text-white pb-5">AMENITIES</p>
-                    </div>
-                    <div className="xl:w-[600px] m-auto">
-                        <p className="text-center font-GTPressura-Light text-4xl text-white px-4">Perfect Balance</p>
-                    </div>
-                    <div className="flex items-center">
-                        <div className="w-auto mx-auto xl:w-1/3">
-                            <p className="text-base font-GTPressura-Light text-center text-white px-4">The Huron amenities offering is exceptional. With its carefully curated 30,000 square feet, designed as a serene sanctuary and a thriving resident community, it flows like the river to the unique pace of life.</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="content-center py-16 pb-20">
-                    <ResponsiveTab 
-                        data={data}
-                    />
-                </div>
+  return (
+    <div>
+      <img src={ BackgroundIamge1} className="inset-0" alt=""/>
+      <div className="px-4 content-center z-10 relative" style={{ backgroundColor: "#2C2A2B" }} >
+        <div className="content-center py-10">
+          <div className="m-auto">
+            <p className="text-sm font-GTPressura-Bold text-center text-white pb-5">AMENITIES</p>
+          </div>
+          <div className="xl:w-[600px] m-auto">
+            <p className="text-center font-GTPressura-Light text-4xl text-white px-4">Perfect Balance</p>
+          </div>
+          <div className="flex items-center">
+            <div className="w-auto mx-auto xl:w-1/3">
+              <p className="text-base font-GTPressura-Light text-center text-white px-4">The Huron amenities offering is exceptional. With its carefully curated 30,000 square feet, designed as a serene sanctuary and a thriving resident community, it flows like the river to the unique pace of life.</p>
             </div>
-            
-            <img src={ BackgroundIamge2}alt="" className="w-full"/>
-            
+          </div>
         </div>
-    )
+        <div className="content-center py-16 pb-20">
+          <ResponsiveTab 
+            data={data}
+          />
+        </div>
+      </div>
+      
+      <img src={ BackgroundIamge2}alt="" className="w-full"/>
+        
+    </div>
+  )
 }
