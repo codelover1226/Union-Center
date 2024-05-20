6e8c0a52b70c92e4ebb2c2466a2db7f22f0d2118
Jay <wmeng0103@gmail.com>
2024-05-20 18:03:26 +0900

    update again

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -119,7 +119,7 @@ export default function Contact() {
         try {
           const formData = new FormData();
           formData.append("from", 'onboarding@resend.dev');
-          formData.append("to", 'wmeng0103@gmail.com');
+          formData.append("to", 'simonxmachine@gmail.com');
           formData.append("subject", 'New Subscriber');
           formData.append("html", emailTemplate);
         
